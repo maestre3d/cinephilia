42fef9b37699d004ffef63c4e9c43490afdb594d
maestre3d <luis.alonso.16@hotmail.com>
2020-11-20 01:43:41 -0700

    Update sandbox

--- watch-list-service/cmd/lambda/movie/create/main.go
+++ watch-list-service/cmd/lambda/movie/create/main.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"log"
 
+	"github.com/google/uuid"
+
 	movieapp "github.com/maestre3d/cinephilia/watch-list-service/internal/application/tracker/movie"
 	"github.com/maestre3d/cinephilia/watch-list-service/internal/infrastructure/tracker/movie/persistence"
 	gonanoid "github.com/matoous/go-nanoid"
@@ -14,15 +16,14 @@ import (
 func main() {
 	ctx := context.Background()
 	movieRepo := persistence.NewInMemoryMovieRepository()
-	creator := movieapp.NewCreator(movieRepo)
-	movieHandler := movieapp.NewCreateCommandHandler(creator)
+	movieHandler := movieapp.NewCreateCommandHandler(movieapp.NewCreator(movieRepo))
 
-	movieId, _ := gonanoid.ID(16)
+	movieId := uuid.New()
 	userId, _ := gonanoid.ID(16)
 	categoryId, _ := gonanoid.ID(16)
 
 	err := movieHandler.Invoke(ctx, movieapp.CreateCommand{
-		Id:          movieId,
+		Id:          movieId.String(),
 		DisplayName: "There will be blood",
 		Description: "Directed by Paul Thomas Anderson. Lead actor: Daniel Day Lewis.",
 		CategoryId:  categoryId,
@@ -32,22 +33,25 @@ func main() {
 		log.Fatal(ddderr.GetDescription(err))
 	}
 
+	movieId = uuid.New()
+
+	err = movieHandler.Invoke(ctx, movieapp.CreateCommand{
+		Id:          movieId.String(),
+		DisplayName: "Blade Runner 2049",
+		Description: "Young Blade Runner K's discovery of a long-buried secret leads him to track down former Blade " +
+			"Runner Rick Deckard, who's been missing for thirty years.",
+		UserId: userId,
+	})
+	if err != nil {
+		log.Fatal(ddderr.GetDescription(err))
+	}
+
 	queryHandler := movieapp.NewFindQueryHandler(movieapp.NewFinder(movieRepo))
-	mov, err := queryHandler.Invoke(ctx, movieapp.FindQuery{Id: movieId})
+	mov, err := queryHandler.Invoke(ctx, movieapp.FindQuery{Id: movieId.String()})
 	if err != nil {
 		log.Fatal(ddderr.GetDescription(err))
 	}
 
 	movieJSON, _ := json.Marshal(mov)
 	log.Print(string(movieJSON))
-	/*
-		err = movieHandler.Invoke(ctx, movieapp.CreateCommand{
-			Id:          movieId,
-			DisplayName: "Thurman Show",
-			Description: "",
-			UserId:      userId,
-		})
-		if err != nil {
-			log.Fatal(ddderr.GetDescription(err))
-		}*/
 }
